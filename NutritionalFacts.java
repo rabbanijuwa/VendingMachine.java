b5e6ecd40f97a2238cb8620ddd6e185a1940ce14
juwarabbani <rabbanijuwa@gmail.com>
2023-09-14 14:08:35 -0400

    Original project

--- NutritionalFacts.java
+++ NutritionalFacts.java
@@ -3,32 +3,49 @@ public class NutritionalFacts {
     private double sugar;
     private double fat;
     private double protein;
+    private ProductCategory productCategory;
 
-    public NutritionalFacts(int calories, double sugar, double fat, double protein) {
+    public NutritionalFacts(int calories, double sugar, double fat, double protein, ProductCategory productCategory) {
         this.calories = calories;
         this.sugar = sugar;
         this.fat = fat;
         this.protein = protein;
+        this.productCategory = productCategory;
     }
     public int getCalories() {
         return calories;
     }
 
+    public void setCalories(){
+        this.calories = calories;
+    }
+
     public double getSugar() {
         return sugar;
+    }
+    public void setSugar(){
+        this.sugar = sugar;
     }
 
     public double getFat() {
         return fat;
     }
+    public void setFat(){
+       this.fat = fat;
+    }
 
     public double getProtein() {
         return protein;
     }
-
-    @Override
-    public String toString() {
-        return "Calories: " + calories + " | Sugar: " + sugar + "g | Fat: " + fat + "g | Protein: " + protein + "g";
+    public void setProtein(){
+        this.protein = protein;
     }
-
+    public ProductCategory getProductCategory(){
+        return productCategory;
+    }
+    public void stBreed(){
+        this.productCategory = productCategory;
+    }
 }
+
+
